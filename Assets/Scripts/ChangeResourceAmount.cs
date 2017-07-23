bd281c52e2510e4b0905f10c05470318da5919fa
jkelvinc <Kelvin@Kelvins-MacBook-Pro.local>
2017-07-23 16:40:02 -0400

    Add script to specify which resource to decrease

---
+++ Assets/Scripts/ChangeResourceAmount.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeResourceAmount : MonoBehaviour 
+{
+	public enum ChangeResourceOperation
+	{
+		Increase,
+		Decrease
+	}
+
+
+	[SerializeField]
+	private ResourceType type;
+
+	[SerializeField]
+	private int amount;
+
+	[SerializeField]
+	private ChangeResourceOperation operation;
+
+
+	public void Trigger()
+	{
+		if (this.operation == ChangeResourceOperation.Increase)
+		{
+			ResourceManager.Instance.IncreaseResource(this.type, this.amount);
+		}
+		else if (this.operation == ChangeResourceOperation.Decrease)
+		{
+			ResourceManager.Instance.DecreaseResource(this.type, this.amount);
+		}
+	}
+}

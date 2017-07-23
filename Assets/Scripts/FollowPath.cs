091e4f50e920e07488288d27cb718429db916563
jkelvinc <Kelvin@Kelvins-MacBook-Pro.local>
2017-07-22 21:51:30 -0400

    Enemies now face the direction they are moving towards

--- Assets/Scripts/FollowPath.cs
+++ Assets/Scripts/FollowPath.cs
@@ -9,6 +9,7 @@ public class FollowPath : MonoBehaviour
 
 	private Path path;
 	private PathNode targetNode;
+	private PathNode previousNode;
 
 
 	public void FixedUpdate()
@@ -17,11 +18,13 @@ public class FollowPath : MonoBehaviour
 		{
 			if (HasReachedTarget())
 			{
+				this.previousNode = this.targetNode;
 				GetNextTargetNode(this.targetNode);
+				UpdateRotation();
 			}
 			else
 			{
-				transform.position = Vector2.MoveTowards(transform.position, this.targetNode.transform.position, speed * Time.fixedDeltaTime);
+				UpdatePosition();
 			}
 		}
 	}
@@ -35,6 +38,7 @@ public class FollowPath : MonoBehaviour
 
 		this.path = path;
 		GetNextTargetNode(null);
+		UpdateRotation();
 	}
 
 	private void GetNextTargetNode(PathNode currentNode)
@@ -46,5 +50,19 @@ public class FollowPath : MonoBehaviour
 	{
 		return (Mathf.Approximately(this.targetNode.transform.position.x, transform.position.x) &&
 				Mathf.Approximately(this.targetNode.transform.position.y, transform.position.y));
+	}
+
+	private void UpdateRotation() 
+	{
+		Vector3 previousPos = (this.previousNode == null) ? transform.position : this.previousNode.transform.position;
+		Vector3 direction = (this.targetNode.transform.position - previousPos).normalized;
+		
+		float rotationAngle = Mathf.Atan2(direction.y, direction.x) * 180f / Mathf.PI;
+		transform.rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
+	}
+
+	private void UpdatePosition()
+	{
+		transform.position = Vector2.MoveTowards(transform.position, this.targetNode.transform.position, speed * Time.fixedDeltaTime);
 	}
 }

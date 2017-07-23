37fb2c78f53f24e9abb7646c1c3433524f166953
jkelvinc <Kelvin@Kelvins-MacBook-Pro.local>
2017-07-23 01:30:06 -0400

    Add Tower script that allow specifying tower damage

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour 
+{
+	[SerializeField]
+	private int attackDamage;
+
+
+	public int AttackDamage
+	{
+		get { return this.attackDamage; }
+	}
+}
